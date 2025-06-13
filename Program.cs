e4aaccf404d1e22e3475a7abe754f183a5cc2892
Albin <albin@bohlin.nu>
2025-06-13 04:22:49 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ov_5_Garage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
